6beb976695c2f045cb4d21ecc08fad6030cf2e9f
pangrongxian <859358014@qq.com>
2022-07-15 16:16:53 +0800

    提交前端代码

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import router from '@/router/index'
+import { key, store } from '@/store'
+import App from './App.vue'
+// import styleImport from '@/utils/style-import'
+import '@/style/basic.styl'
+
+const app = createApp(App)
+
+
+app.use(router).use(store, key).mount('#app')
+
+// styleImport(app).use(router).use(store, key).mount('#app')
